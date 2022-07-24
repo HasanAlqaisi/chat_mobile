817988766af08245cf844f6533031b199dda90df
HasanAlQaisi <hasan.alqaisi2000@gmail.com>
2022-07-24 21:17:43 +0300

    add: desired path for profile image holder

--- lib/utils/constants/assets_path.dart
+++ lib/utils/constants/assets_path.dart
@@ -2,4 +2,5 @@ abstract class AssetsPath {
   static const loginImage = "assets/images/login_image.svg";
   static const signupImage = "assets/images/signup_image.svg";
   static const otpImage = "assets/images/otp_image.svg";
+  static const profilePlaceHolder = "assets/images/profile_placeholder.png";
 }
